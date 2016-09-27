8b4a3514a6a538e35762e94d087271f16b94fb36
euklid86 <euklid86@gmail.com>
2016-09-27 16:38:45 +0300

    Add files via upload

---
+++ 27-9-2016/Exercise 2.py
@@ -0,0 +1,8 @@
+fiftys=input("Enter number of 50 euro banknotes:")
+twentys=input("Enter number of 20 euro banknotes:")
+tens=input("Enter number of 10 euro banknotes:")
+fives=input("Enter number of 5 euro banknotes:")
+twos=input("Enter number of 2 euro coins:")
+ones=input("Enter number of 1 euro coins:")
+total=int(fiftys)*50+ int(twentys)*20+int(tens)*10+int(fives)*5+int(twos)*2+int(ones)
+print("You have", total,"euros")
