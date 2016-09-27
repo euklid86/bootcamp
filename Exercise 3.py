f2b87069fe5a7f0a7e07a57e762fdc9334d8dde4
euklid86 <euklid86@gmail.com>
2016-09-27 16:39:16 +0300

    Delete Exercise 3.py

--- Exercise 3.py
+++
@@ -1,8 +0,0 @@
-import math
-a=float(input("Please give the a side: "))
-b=float(input("Please give the b side: "))
-c=float(input("Please give the c side: "))
-subtotal=(a+b+c)*(b+c-a)*(a-b+c)*(a+b-c)
-ssubtotal=math.sqrt(subtotal)
-total=1/4 * ssubtotal
-print("The area of the triangle is :", total)
