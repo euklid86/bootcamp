3e87a98a5b0a72a0e0f5297af90e4e84be5a674f
euklid86 <euklid86@gmail.com>
2017-01-15 00:10:38 +0200

    Delete Exercises 03-10-2016.sql

--- MySql/Exercises 03-10-2016.sql
+++
@@ -1,6 +0,0 @@
-insert into bootcamp.courses (coursesid, `Name`, Lecturer, Assistant, Duration, `Starting Date`)
-values (1, "SQL", "AG", "BN", 2, '2016-10-21'),
-	   (2, "Java", "VS", "BN", 3, '2016-10-25'),
-       (3, "R", "KP", "TT", 4, '2016-11-09'),
-       (4, "Python", "KK", "DD", 5, '2016-11-09');
-select distinct Assistant from bootcamp.courses;
